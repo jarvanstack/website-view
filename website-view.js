52c4c4b6bc201836dbeeb9186399f5448bf1870f
dengjiawen <dengjiawen8955@gmail.com>
2023-03-06 21:25:19 +0800

    fix: Fix visiable problem

--- website-view.js
+++ website-view.js
@@ -5,32 +5,32 @@ let WebsiteUV = 0;
 // Get the span elements where the PV and UV will be displayed
 
 
-function updatePageViews() {
+async function updatePageViews() {
+    try {
+        // Get the span And Container elements where the PV and UV will be displayed
+        const pvContainer = document.getElementById("website_pv_container");
+        const uvContainer = document.getElementById("website_uv_container");
+        const pvSpan = document.getElementById("website_pv");
+        const uvSpan = document.getElementById("website_uv");
+        const website = window.location.hostname;
+        // Create the data object with the website name
+        const data = {
+            website: website
+        };
 
-    // Get the span And Container elements where the PV and UV will be displayed
-    const pvContainer = document.getElementById("website_pv_container");
-    const uvContainer = document.getElementById("website_uv_container");
-    const pvSpan = document.getElementById("website_pv");
-    const uvSpan = document.getElementById("website_uv");
-    const website = window.location.hostname;
-  // Create the data object with the website name
-    const data = {
-        website: website
-    };
-
-  // Send the API request to add a PV and get the response
-    fetch("https://api1.bmft.tech/dc3/api_platform/add_view", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Update the PV and UV spans with the values from the response
-        WebsitePV = data.data.pv;
-        WebsiteUV = data.data.uv;
+        // Send the API request to add a PV and get the response
+        if (WebsitePV == 0) {
+            const response = await fetch("https://api1.bmft.tech/dc3/api_platform/add_view", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+            const resp = await response.json();
+            WebsitePV = resp.data.pv;
+            WebsiteUV = resp.data.uv;
+        }
 
         // If Span is not null, update the PV and UV spans
         if (pvSpan !== null) {
@@ -47,10 +47,15 @@ function updatePageViews() {
         if (uvContainer !== null) {
             uvContainer.style.display = "block";
         }
-    })
-    .catch(error => console.error(error));
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 // Call the function to update the PV and UV spans when the page is loaded
 window.addEventListener("load", updatePageViews);
+window.addEventListener("focus", updatePageViews);
+window.addEventListener("resize", updatePageViews);
+window.addEventListener("scroll", updatePageViews);
+window.addEventListener("blur", updatePageViews);
 
