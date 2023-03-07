6d29b4fc72972053399fc1a6160fd5e4719690fd
dengjiawen <dengjiawen8955@gmail.com>
2023-03-07 13:47:12 +0800

    fix: Fix Duplicate Requsts

--- website-view.js
+++ website-view.js
@@ -4,6 +4,27 @@ let WebsiteUV = 0;
 
 // Get the span elements where the PV and UV will be displayed
 
+async function onceFetchAPi() {
+    console.log('onceFetchAPi');
+    const website = window.location.hostname;
+    // Create the data object with the website name
+    const data = {
+        website: website
+    };
+    const response = await fetch("https://api1.bmft.tech/dc3/api_platform/add_view", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    const resp = await response.json();
+    WebsitePV = resp.data.pv;
+    WebsiteUV = resp.data.uv;
+    await updatePageViews();
+}
+
+var fetched = false;
 
 async function updatePageViews() {
     try {
@@ -12,50 +33,56 @@ async function updatePageViews() {
         const uvContainer = document.getElementById("website_uv_container");
         const pvSpan = document.getElementById("website_pv");
         const uvSpan = document.getElementById("website_uv");
-        const website = window.location.hostname;
-        // Create the data object with the website name
-        const data = {
-            website: website
-        };
-
         // Send the API request to add a PV and get the response
-        if (WebsitePV == 0) {
-            const response = await fetch("https://api1.bmft.tech/dc3/api_platform/add_view", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            })
-            const resp = await response.json();
-            WebsitePV = resp.data.pv;
-            WebsiteUV = resp.data.uv;
+        if (WebsitePV == 0 && !fetched) {
+            // only exec one time
+            fetched = true;
+            await onceFetchAPi();
+        }
+        for (let i = 0; i < 2; i++) {
+            // If Span is not null, update the PV and UV spans
+            if (pvSpan !== null) {
+                pvSpan.textContent = WebsitePV;
+            }
+            if (uvSpan !== null) {
+                uvSpan.textContent = WebsiteUV;
+            }
+
+            // If Container is not null, style='display:none' set to 'display:block'
+            if (pvContainer !== null) {
+                pvContainer.style.display = "block";
+            }
+            if (uvContainer !== null) {
+                uvContainer.style.display = "block";
+            }
+
+            // sleep 100ms
+            await new Promise(resolve => setTimeout(resolve, 10));
         }
 
-        // If Span is not null, update the PV and UV spans
-        if (pvSpan !== null) {
-            pvSpan.textContent = WebsitePV;
-        }
-        if (uvSpan !== null) {
-            uvSpan.textContent = WebsiteUV;
-        }
-
-        // If Container is not null, style='display:none' set to 'display:block'
-        if (pvContainer !== null) {
-            pvContainer.style.display = "block";
-        }
-        if (uvContainer !== null) {
-            uvContainer.style.display = "block";
-        }
     } catch (error) {
         console.error(error);
     }
 }
 
 // Call the function to update the PV and UV spans when the page is loaded
-window.addEventListener("load", updatePageViews);
-window.addEventListener("focus", updatePageViews);
-window.addEventListener("resize", updatePageViews);
-window.addEventListener("scroll", updatePageViews);
-window.addEventListener("blur", updatePageViews);
+window.addEventListener("load", function () {
+    console.log('load');
+    updatePageViews();
+});
+// window.addEventListener("focus", updatePageViews);
+// window.addEventListener("resize", updatePageViews);
+// window.addEventListener("scroll", function () {
+//     console.log('scroll');
+//     updatePageViews();
+// });
+// window.addEventListener("blur", updatePageViews);
 
+window.addEventListener('hashchange', function (event) {
+    console.log('hashchange');
+    updatePageViews();
+})
+// window.addEventListener('popstate', function (event) {
+//     console.log('popstate');
+//     updatePageViews();
+// })
